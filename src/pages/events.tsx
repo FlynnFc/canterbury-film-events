aefa027b26cb129b8d4a99040b05de924fb4ef30
Flynn Foley-Clarke <mastare.flynn@gmail.com>
2022-06-29 05:12:14 +0100

    button linking to create page

--- src/pages/events.tsx
+++ src/pages/events.tsx
@@ -1,6 +1,16 @@
+import Link from 'next/link';
 import React from 'react';
 
 const Events = () => {
-  return <div>sads</div>;
+  return (
+    <div className="mt-24 flex justify-center">
+      {' '}
+      <Link rel="events" href={`events/create`} key="create">
+        <a className="rounded bg-black p-3  px-4 font-medium uppercase text-white">
+          Create new event
+        </a>
+      </Link>
+    </div>
+  );
 };
 export default Events;
