42252ccf0ddf1e25e26240b8facf6d58d58ed16c
Flynn Foley-Clarke <mastare.flynn@gmail.com>
2022-06-29 05:09:30 +0100

    form added, 404 page made better

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,7 +9,14 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang={AppConfig.locale}>
-        <Head></Head>
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
 
         <body className="bg-slate-100">
           <Navbar />
---
+++ src/pages/events/create.tsx
@@ -0,0 +1,91 @@
+import { useState } from 'react';
+// import { useState } from 'react';
+
+const Create = () => {
+  const [currentTitle, setCurrentTitle] = useState('');
+
+  return (
+    <div className="mx-auto my-4 flex max-w-2xl flex-col rounded-2xl bg-white p-10">
+      <div className="mb-10 space-y-2">
+        <h1 className="text-2xl font-semibold">Creating a new event</h1>
+        <p className="text-slate-400">
+          Once you create your event a moderator will verify its legitimacy and
+          then publish to the events page. This can sometimes take 2-3 days.
+        </p>
+      </div>{' '}
+      <form
+        action={`/${currentTitle}`}
+        method="post"
+        className="flex flex-col space-y-2"
+      >
+        <label htmlFor="title" className="text-xl font-medium">
+          Event Title
+        </label>
+        <input
+          required
+          onChange={(e) => setCurrentTitle(e.target.value)}
+          type="text"
+          className="rounded-lg border-2 border-gray-300 p-2"
+          placeholder="event title"
+        />
+        <label htmlFor="last" className="text-xl font-medium">
+          Description
+        </label>
+        <input
+          required
+          type="text"
+          className="rounded-lg border-2 border-gray-300 p-2"
+          placeholder="description "
+        />
+        <div className="flex w-full flex-col">
+          <label htmlFor="first" className="mt-2 text-xl font-medium">
+            Contact email
+          </label>
+          <p className="text-slate-400">This will not be shared</p>
+          <input
+            required
+            type="email"
+            className="mt-2 rounded-lg border-2 border-gray-300 p-2"
+            placeholder="email"
+          />
+        </div>
+        <div id="socials">
+          <div className="flex w-full flex-col">
+            <label htmlFor="first" className="mt-2 text-xl font-medium">
+              Socials
+            </label>
+            <div className="mb-4 flex w-full flex-wrap justify-start">
+              <input
+                type="url"
+                className="box-border w-full  rounded-t-lg border-2 border-b-0 border-gray-300 p-2"
+                placeholder="instagram"
+              />
+              <input
+                type="url"
+                className="box-border w-full   border-2 border-gray-300 p-2"
+                placeholder="facebook"
+              />
+              <input
+                type="url"
+                className="box-border w-full  border-x-2  border-gray-300 p-2"
+                placeholder="twitter"
+              />
+              <input
+                type="url"
+                className="box-border w-full  rounded-b-lg border-2 border-gray-300 p-2"
+                placeholder="website"
+              />
+            </div>
+          </div>
+        </div>
+        <button
+          type="submit"
+          className="rounded-lg border-2 border-black bg-green-500 p-2"
+        >
+          Continue
+        </button>
+      </form>
+    </div>
+  );
+};
+export default Create;
